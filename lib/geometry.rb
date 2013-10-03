ac695e7ed5ec71abf4c9e4dcd6f375de6039a636
Abby Howell <abby.g.howell@gmail.com>
2013-10-03 18:14:43 -0400

    finished

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,29 +1,68 @@
 module Geometry
 
 	class Triangle
-		attr_accessor :sideA, :sideB, :sideC, :sides
+		attr_accessor :a, :b, :c, :sides
 
 		def initialize(sideA, sideB, sideC)
-			@sideA = sideA
-			@sideB = sideB
-			@sideC = sideC
-
-			@sides = [@sideA, @sideB, @sideC]
+			@a = sideA
+			@b = sideB
+			@c = sideC
+			@sides = [@a, @b, @c]
 			sides.sort!
+		end
+
+		def valid?
+				if @sides[0] + @sides[1] <= @sides[2]
+				false
+			else 
+				true
+			end
 		end
 
 		def perimeter
 			self.sides.inject(0) {|sum, side| sum + side }
 		end
 
-
+		def area
+			p = (self.perimeter / 2.0)
+			Math.sqrt( p * (p - @a) * (p - @b) * (p - @c) )
+		end
 
 	end
 
 	class Rectangle
+		attr_accessor :length, :width
+
+		def initialize(length, width)
+			@length = length
+			@width = width
+		end
+
+		def area
+			@length * @width
+		end
+
+		def perimeter
+			(@length * 2) + (@width * 2)
+		end
 	end
 
 	class Circle
+		attr_accessor :radius
+
+		def initialize(radius)
+			@radius = radius
+		end
+
+		def area
+			(@radius ** 2) * Math::PI
+		end
+
+		def perimeter
+			(@radius * 2) * Math::PI
+		end
+
 	end
 
 end
+
--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,18 +1,96 @@
 require 'spec_helper'
 require_relative '../lib/geometry'
 
-describe Geometry::Triangle, "#Perimeter" do
-  
-  it "should have a method called perimeter" do
-    should respond_to :perimeter
-  end
+describe Geometry::Triangle do
 
-  it "should return a numeric value when called on a triangle object"
-    a_triangle = Geometry::Triangle.new(3, 4, 5)
-    p = a_triangle.perimeter
-    p.should eq 12
-  end
-  
+# as defined by me, for testing:
+  let(:a) {3}
+  let(:b) {4}
+  let(:c) {5}
+  let(:perimeter) {12}
+  let(:area) {6}
+
+# create new triangle using my code, and test it against
+# what I state to be true, above:
+
+  subject {Geometry::Triangle.new(3, 4, 5)}
+
+  it "should have a perimeter" do
+		subject.perimeter.should eq perimeter
+	end
+
+	it "should have an area" do
+		subject.area.should eq area
+	end
+
+	it "should be valid" do
+		subject.valid?.should be true
+	end
+
+	# test attr_accessor variables:
+
+	its(:a) { should eq 3}
+	its(:b) { should eq 4}
+	its(:c) { should eq 5}
+
+# test methods by calling self.perimeter and comparing it
+# to the values defined in "let" above:
+	its(:perimeter) { should eq perimeter}
+	its(:area) { should eq area}
+
+	its(:sides) { should eq [a, b, c]}
+
+	# non_example {Geometry::Triangle.new(2, 1, 8)}
+
+	# it "should be invalid' " do
+	# 	non_example.valid?.should be false
+	# end
 
 end
+
+describe Geometry::Rectangle do
+	let(:length) {6}
+	let(:width) {4}
+	let(:perimeter) {20}
+	let(:area) {24}
+
+	subject{Geometry::Rectangle.new(6, 4)}
+
+	it "should have a perimeter" do
+		subject.perimeter.should eq perimeter
+	end
+
+	it "should have an area" do
+		subject.area.should eq area
+	end
+
+	its(:length) { should eq 6}
+	its(:width) { should eq 4 }
+
+	its(:perimeter) { should eq 20}
+	its(:area) { should eq 24 }
+
+end
+
+describe Geometry::Circle do
+
+	let(:radius) {5}
+	let(:area) {25 * Math::PI}
+	let(:perimeter) {10 * Math::PI}
+
+	subject{Geometry::Circle.new(5)}
+
+	it "should have a perimeter" do
+	  subject.perimeter.should eq perimeter
+	end
+
+	it "should have an area" do
+		subject.area.should eq area
+	end
+
+	its(:radius) {should eq 5}
+
+end
+
+
 
