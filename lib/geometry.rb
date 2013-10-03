c14caa7d78d279eabc7d95ebb3998d663f633120
Abby Howell <abby.g.howell@gmail.com>
2013-10-03 15:00:39 -0400

    ongoing work on testing

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,3 +1,29 @@
 module Geometry
 
+	class Triangle
+		attr_accessor :sideA, :sideB, :sideC, :sides
+
+		def initialize(sideA, sideB, sideC)
+			@sideA = sideA
+			@sideB = sideB
+			@sideC = sideC
+
+			@sides = [@sideA, @sideB, @sideC]
+			sides.sort!
+		end
+
+		def perimeter
+			self.sides.inject(0) {|sum, side| sum + side }
+		end
+
+
+
+	end
+
+	class Rectangle
+	end
+
+	class Circle
+	end
+
 end
--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 require_relative '../lib/geometry'
 
-describe "#Geometry" do
-
-	it "should have a module called Geometry" do
-	end
-
-end
-
-describe "#Triangle" do
+describe Geometry::Triangle, "#Perimeter" do
   
-  it "should have a method called Perimeter" do
+  it "should have a method called perimeter" do
+    should respond_to :perimeter
   end
 
-  it "should have a method called Area" do
+  it "should return a numeric value when called on a triangle object"
+    a_triangle = Geometry::Triangle.new(3, 4, 5)
+    p = a_triangle.perimeter
+    p.should eq 12
   end
+  
 
 end
+
